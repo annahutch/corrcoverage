76713386a71a839422950e30e74a371de0caf055
Anna Hutchinson <annahutchinson1995@gmail.com>
2019-03-11 16:45:24 +0000

    corrected_cs

--- R/corrected_cs.R
+++ R/corrected_cs.R
@@ -4,6 +4,7 @@
 #' @param f Minor allele frequencies
 #' @param N0 Number of controls
 #' @param N1 Number of cases
+#' @param Sigma Correlation matrix of SNPs
 #' @param lower Lower threshold
 #' @param upper Upper threshold
 #' @param desired.cov The desired coverage of the causal variant in the credible set
@@ -12,19 +13,32 @@
 #'
 #' @return list of variants in credible set, required threshold, the corrected coverage and the size of the credible set
 #' @export
-corrected_cs <- function(z, f, N0, N1, lower, upper, desired.cov, acc = 0.0005, max.iter = 20){
+corrected_cs <- function(z, f, N0, N1, Sigma, lower, upper, desired.cov, acc = 0.0005, max.iter = 20){
   # lower <- 2*desired.cov - 1
   # upper <- min(1,desired.cov + 0.05)
   s = N1/(N0+N1) # proportion of cases
   V = 1/(2 * (N0+N1) * f * (1 - f) * s * (1 - s))
+  W = 0.2
+  r <- W^2/(W^2 + V)
   pp <- ppfunc(z, V = V) # pp of system in question
   muhat = est_mu(z, f, N0, N1)
   nsnps = length(pp)
-  temp <- diag(x = muhat, nrow = nsnps, ncol = nsnps)
-  zj <- do.call(c, apply(temp, 1, list))  # nsnp zj vectors for each snp considered causal
+  temp = diag(x = muhat, nrow = nsnps, ncol = nsnps)
+  zj = lapply(seq_len(nrow(temp)), function(i) temp[i,]) # nsnp zj vectors for each snp considered causal
+  # simulate ERR matrix
 
+  ERR = mvtnorm:::rmvnorm(1000,rep(0,ncol(Sigma)),Sigma)
+  pp_ERR = function(Zj, nrep = 1000, Sigma){
+    exp.zm = Zj %*% Sigma
+    mexp.zm = matrix(exp.zm, 1000, length(Zj), byrow=TRUE) # matrix of Zj replicated in each row
+    zstar = mexp.zm+ERR
+    bf = 0.5 * (log(1 - r) + (r * zstar^2))
+    denom = coloc:::logsum(bf)  # logsum(x) = max(x) + log(sum(exp(x - max(x)))) so sum is not inf
+    pp.tmp = exp(bf - denom)  # convert back from log scale
+    pp.tmp / rowSums(pp.tmp)
+  }
   # simulate pp systems
-  pps <- mapply(zj_pp, zj, V = V, MoreArgs = list(nrep = 1000, Sigma = LD), SIMPLIFY = FALSE)
+  pps <- mapply(pp_ERR, zj, MoreArgs = list(nrep = 1000, Sigma = Sigma), SIMPLIFY = FALSE)
 
   n_pps <- length(pps)
   args <- 1:length(pp)
@@ -74,6 +88,7 @@ corrected_cs <- function(z, f, N0, N1, lower, upper, desired.cov, acc = 0.0005, 
 #' @param V Prior variance of estimated effect sizes
 #' @param N0 Number of controls
 #' @param N1 Number of cases
+#' @param Sigma Correlation matrix of SNPs
 #' @param lower Lower threshold
 #' @param upper Upper threshold
 #' @param desired.cov The desired coverage of the causal variant in the credible set
@@ -82,18 +97,31 @@ corrected_cs <- function(z, f, N0, N1, lower, upper, desired.cov, acc = 0.0005, 
 #'
 #' @return list of variants in credible set, required threshold, the corrected coverage and the size of the credible set
 #' @export
-corrected_cs_bhat <- function(bhat, V, N0, N1, lower, upper, desired.cov, acc = 0.0005, max.iter = 20){
+corrected_cs_bhat <- function(bhat, V, N0, N1, Sigma, lower, upper, desired.cov, acc = 0.0005, max.iter = 20){
   # lower <- 2*desired.cov - 1
   # upper <- min(1,desired.cov + 0.05)
   z = bhat/sqrt(V)
+  W = 0.2
+  r <- W^2/(W^2 + V)
   pp = ppfunc(z, V = V) # pp of system in question
-  muhat = est_mu(z, f, N0, N1)
+  muhat = est_mu_bhat(bhat, V, N0, N1, W = 0.2)
   nsnps = length(pp)
   temp = diag(x = muhat, nrow = nsnps, ncol = nsnps)
-  zj = do.call(c, apply(temp, 1, list))  # nsnp zj vectors for each snp considered causal
+  zj = lapply(seq_len(nrow(temp)), function(i) temp[i,]) # nsnp zj vectors for each snp considered causal
+  # simulate ERR matrix
 
+  ERR = mvtnorm:::rmvnorm(1000,rep(0,ncol(Sigma)),Sigma)
+  pp_ERR = function(Zj, nrep = 1000, Sigma){
+    exp.zm = Zj %*% Sigma
+    mexp.zm = matrix(exp.zm, 1000, length(Zj), byrow=TRUE) # matrix of Zj replicated in each row
+    zstar = mexp.zm+ERR
+    bf = 0.5 * (log(1 - r) + (r * zstar^2))
+    denom = coloc:::logsum(bf)  # logsum(x) = max(x) + log(sum(exp(x - max(x)))) so sum is not inf
+    pp.tmp = exp(bf - denom)  # convert back from log scale
+    pp.tmp / rowSums(pp.tmp)
+  }
   # simulate pp systems
-  pps <- mapply(zj_pp, zj, V = V, MoreArgs = list(nrep = 1000, Sigma = LD), SIMPLIFY = FALSE)
+  pps <- mapply(pp_ERR, zj, MoreArgs = list(nrep = 1000, Sigma = Sigma), SIMPLIFY = FALSE)
 
   n_pps <- length(pps)
   args <- 1:length(pp)
